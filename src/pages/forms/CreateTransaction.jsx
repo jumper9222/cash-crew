b20162e8ee757344f473461194a774bfac864746
jumper9222 <chindidken@gmail.com>
2025-03-31 01:14:18 +0800

    Add image compression functionality and cleanup in CreateTransaction form

--- src/pages/forms/CreateTransaction.jsx
+++ src/pages/forms/CreateTransaction.jsx
@@ -108,6 +108,14 @@ export default function CreateTransaction({ show, onHide }) {
     }, [])
 
     useEffect(() => {
+        return () => {
+            if (thumbnail) {
+                URL.revokeObjectURL(thumbnail)
+            }
+        }
+    }, [thumbnail])
+
+    useEffect(() => {
         if (splitType === "Equally") {
             divideAmountsEqually();
         }
@@ -251,6 +259,87 @@ export default function CreateTransaction({ show, onHide }) {
         fileInputRef.current.click()
     }
 
+    //Recursive image compressor
+    const compressImage = async (file, maxSize) => {
+        return new Promise((resolve) => {
+            //Define image to draw on canvas
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(file);
+
+            img.onload = async () => {
+                //Define canvas and canvas context to reduce uploaded image resolution. 
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                //Get original image resolution
+                let width = img.width;
+                let height = img.height;
+
+                //Calculate original image megapixel
+                const inputMP = width * height;
+
+                //Calculate max image megapizel
+                const maxMP = 1920 * 1080;
+
+                //Downscale image resolution if original MP is bigger than max MP
+                if (inputMP > maxMP) {
+                    //Calculate resolution scale using formula
+                    const mpScale = Math.sqrt((maxMP / inputMP))
+
+                    //Set new resolution retaining aspect ratio
+                    width = width * mpScale
+                    height = height * mpScale
+                }
+
+                //Set canvas width and height before drawing image
+                canvas.width = width;
+                canvas.height = height;
+
+                //Draw image on canvas
+                ctx.drawImage(img, 0, 0, width, height)
+
+                //Set image quality
+                let imageQuality = 1
+                let blob = await new Promise((resolve) =>
+                    canvas.toBlob(resolve, 'image/jpeg', imageQuality)
+                );
+
+                //Compress image if file size is too large
+                while (blob.size >= maxSize && imageQuality > 0.1) {
+                    imageQuality -= 0.1;
+                    blob = await new Promise((resolve) =>
+                        canvas.toBlob(resolve, 'image/jpeg', imageQuality)
+                    )
+                }
+
+                URL.revokeObjectURL(img.src);
+                resolve(blob);
+            };
+        });
+    };
+
+    const setImageAndThumbnail = (image) => {
+        setImage(image);
+        setThumbnail(URL.createObjectURL(image))
+    }
+
+    //Image field handler
+    const handleImageChange = async (e) => {
+        const file = e.target.files[0];
+        const maxSize = 200 * 1024
+        if (file) {
+            console.log(file.name)
+            if (file.size > maxSize) {
+                const compressedImageBlob = await compressImage(file, maxSize);
+                console.log(compressedImageBlob)
+                const compressedImageFile = new File([compressedImageBlob], file.name, { type: compressedImageBlob.type, lastModified: Date.now() });
+                setImageAndThumbnail(compressedImageFile)
+            } else {
+                setImageAndThumbnail(file)
+            }
+        }
+    }
+
     return (
         <>
             <Modal show={show === 'add-transaction'} onHide={onHide} centered>
@@ -336,23 +425,17 @@ export default function CreateTransaction({ show, onHide }) {
                                 required
                             />
                         </Form.Group>
-                        {thumbnail && <Image className="mb-3" width='50px' src={thumbnail} onClick={() => setShowModal(true)} fluid />}
+                        {thumbnail &&
+                            <div style={{ width: '50px', height: '50px', overflow: "hidden" }} className="mb-3 d-flex justify-content-center align-items-center">
+                                <Image width='50px' src={thumbnail} onClick={() => setShowModal(true)} fluid />
+                            </div>
+                        }
                         <Form.Group className="mb-2">
                             <Form.Control
                                 className="d-none"
                                 type="file"
-                                onChange={(e) => {
-                                    console.log(e.target.files)
-                                    const file = e.target.files[0]
-                                    setImage(file)
-                                    if (file) {
-                                        const reader = new FileReader();
-                                        reader.onloadend = () => {
-                                            setThumbnail(reader.result)
-                                        };
-                                        reader.readAsDataURL(file)
-                                    }
-                                }}
+                                accept="image/*"
+                                onChange={handleImageChange}
                                 ref={fileInputRef}
                             />
                             <Button onClick={handleAddImage}>Add Image</Button>
